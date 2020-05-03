8ce460ea82671ddfeae32313042a4ed63c5881d8
Pradeepan <pradeepan.prabha@gmail.com>
2020-05-03 21:28:26 +0530

    style: Comment reviewed v0.0.1
    Vocabulary Search Results checked

--- bikeshare.py
+++ bikeshare.py
@@ -10,8 +10,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
-
+    Asks user to specify a city, month, and day of week to analyze.
+    
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
